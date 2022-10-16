991e6b6af1d547a02ddb09d368ee17c661f29190
Nikita Prodan <sa_vvk@mail.ru>
2022-10-16 18:19:43 +0300

    Add replace plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,9 +5,13 @@ import fileInclude from 'gulp-file-include';
 // Pathes
 import { folderPath } from '../config/path.js';
 
+// Plugins
+import { plugins } from '../config/plugins.js';
+
 export function html() {
   return gulp
     .src(folderPath.src.html)
     .pipe(fileInclude())
+    .pipe(plugins.replace(/@img\//g, 'images/'))
     .pipe(gulp.dest(folderPath.build.html));
 }
