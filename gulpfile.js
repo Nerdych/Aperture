bee6fcdb932ba5007b12710ebd9f60356ec7eee9
Nikita Prodan <sa_vvk@mail.ru>
2022-10-16 16:51:12 +0300

    Add package for delete and include html

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,17 @@ import gulp from 'gulp';
 import { folderPath } from './gulp/config/path.js';
 
 // Tasks
+import { reset } from './gulp/tasks/reset.js';
 import { copy } from './gulp/tasks/copy.js';
+import { html } from './gulp/tasks/html.js';
 
 function watcher() {
   gulp.watch(folderPath.watch.files, copy);
+  gulp.watch(folderPath.watch.html, html);
 }
 
-const dev = gulp.series(copy, watcher);
+const mainTasks = gulp.parallel(copy, html);
+
+const dev = gulp.series(reset, mainTasks, watcher);
 
 gulp.task('default', dev);
