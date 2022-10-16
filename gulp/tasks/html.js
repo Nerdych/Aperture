02f9c8f03a44f8f427884fc42a73a72e2c328ba1
Nikita Prodan <sa_vvk@mail.ru>
2022-10-16 18:29:59 +0300

    Add version control plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,8 @@
 // Core
 import gulp from 'gulp';
 import fileInclude from 'gulp-file-include';
+import webpHtmlNoSvg from 'gulp-webp-html-nosvg';
+import versionNumber from 'gulp-version-number';
 
 // Pathes
 import { folderPath } from '../config/path.js';
@@ -13,5 +15,19 @@ export function html() {
     .src(folderPath.src.html)
     .pipe(fileInclude())
     .pipe(plugins.replace(/@img\//g, 'images/'))
+    .pipe(webpHtmlNoSvg())
+    .pipe(
+      versionNumber({
+        value: '%DT%',
+        append: {
+          key: '_v',
+          cover: 0,
+          to: ['css', 'js'],
+        },
+        output: {
+          file: 'gulp/version.json',
+        },
+      })
+    )
     .pipe(gulp.dest(folderPath.build.html));
 }
