1f58f7e2e6b740f63c983d4a170034561d842d52
Nikita Prodan <sa_vvk@mail.ru>
2022-10-16 16:50:28 +0300

    Add path for html files

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,10 +1,10 @@
 import * as path from 'path';
 
 const rootFolder = path.basename(path.resolve());
+const __dirname = path.resolve();
 
 const buildFolder = './dist';
 const srcFolder = './src';
-const __dirname = path.resolve();
 
 function replaceBackslash(path) {
   return path.replaceAll('\\', '/');
@@ -12,17 +12,20 @@ function replaceBackslash(path) {
 
 export const folderPath = {
   build: {
+    html: replaceBackslash(path.resolve(__dirname, buildFolder)),
     files: replaceBackslash(path.resolve(__dirname, buildFolder, 'files')),
   },
   src: {
     files: replaceBackslash(
       path.resolve(__dirname, srcFolder, 'files', '**', '*.*')
     ),
+    html: replaceBackslash(path.resolve(__dirname, srcFolder, '*.html')),
   },
   watch: {
     files: replaceBackslash(
       path.resolve(__dirname, srcFolder, 'files', '**', '*.*')
     ),
+    html: replaceBackslash(path.resolve(__dirname, srcFolder, '**', '*.html')),
   },
   clean: buildFolder,
   srcFolder: srcFolder,
