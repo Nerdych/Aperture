6de5478a295de0d8ec7162a783514f73394cbcdd
Nikita Prodan <sa_vvk@mail.ru>
2022-10-24 18:45:33 +0300

    Edit js

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,6 +3,5 @@ const $nav = document.querySelector('#nav');
 
 $menuButton.addEventListener('click', () => {
   $menuButton.classList.toggle('header__menu--active');
-
   $nav.classList.toggle('header__nav--active');
 });
