205de2030c56e6ece59c4db1d3abf54c810d6f67
Nikita Prodan <sa_vvk@mail.ru>
2022-10-24 22:08:54 +0300

    Create new build

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,8 +1,1 @@
-"use strict";
-
-const $menuButton = document.querySelector('#menu-button');
-const $nav = document.querySelector('#nav');
-$menuButton.addEventListener('click', () => {
-  $menuButton.classList.toggle('header__menu--active');
-  $nav.classList.toggle('header__nav--active');
-});
+"use strict";const $menuButton=document.querySelector("#menu-button"),$nav=document.querySelector("#nav");$menuButton.addEventListener("click",(()=>{$menuButton.classList.toggle("header__menu--active"),$nav.classList.toggle("header__nav--active")}));
