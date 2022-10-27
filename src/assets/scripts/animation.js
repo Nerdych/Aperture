07c46de7994107f9fb69189016301afca0fc880b
Nikita Prodan <sa_vvk@mail.ru>
2022-10-27 23:10:58 +0300

    Change animations

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -3,60 +3,47 @@ import gsap from 'gsap';
 
 const tl = gsap.timeline();
 
-function pageAnimateIn(container) {
+const $menuButton = document.querySelector('#menu-button');
+const $nav = document.querySelector('#nav');
+
+function pageAnimateIn(container, text) {
+  const $loader = container.querySelector('.loader');
+  const $loaderText = container.querySelector('.loader__text');
+  $loaderText.innerHTML = text;
   return tl
-    .to(container.querySelector('.circle'), {
+    .to($loader, {
       duration: 0.5,
       width: '100%',
-      fontSize: '60px',
-      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
     })
-    .to(container.querySelector('.circle'), {
-      duration: 3,
+    .to($loaderText, {
+      duration: 0.5,
+      delay: 0.25,
+      opacity: 1,
+      transform: 'translate(-50%, -50%)',
+    })
+    .to($loaderText, {
+      duration: 0.5,
+      delay: 0.25,
+      opacity: 1,
+      transform: 'translate(-50%, -50%)',
+    })
+    .to($loaderText, {
+      duration: 0.5,
+      delay: 0.25,
+      opacity: 0,
+      transform: 'translate(-50%, -150%)',
     });
-}
-
-function animateContent(container) {
-  return tl
-    .fromTo(
-      container.querySelector('h1'),
-      {
-        translateX: -100,
-        opacity: 0,
-      },
-      {
-        translateX: 0,
-        opacity: 1,
-      }
-    )
-    .fromTo(
-      container.querySelector('p'),
-      {
-        translateY: 40,
-        opacity: 0,
-        stagger: 0.2,
-      },
-      {
-        translateY: 0,
-        opacity: 1,
-        stagger: 0.2,
-      }
-    );
 }
 
 async function pageAnimateOut(container) {
   await tl.fromTo(
-    container.querySelector('.circle'),
+    container.querySelector('.loader'),
     {
       duration: 0.5,
       width: '100%',
-      fontSize: '60px',
-      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
     },
     {
       width: '0',
-      fontSize: '0px',
-      clipPath: 'polygon(0 0, 80% 0, 100% 100%, 0 100%)',
     }
   );
 }
@@ -66,15 +53,13 @@ barba.init({
     {
       name: 'opacity-transition',
       async leave(data) {
-        await pageAnimateIn(data.current.container);
+        $menuButton.classList.remove('header__menu--active');
+        await pageAnimateIn(data.current.container, data.trigger.dataset.loader);
+        $nav.classList.remove('header__nav--active');
         data.current.container.remove();
       },
       async enter(data) {
         await pageAnimateOut(data.next.container);
-        await animateContent(data.next.container);
-      },
-      async once(data) {
-        await animateContent(data.next.container);
       },
     },
   ],
