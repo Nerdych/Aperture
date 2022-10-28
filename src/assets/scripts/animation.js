7e5d7d5930b2721f392a67a4d7eac74d42546e4e
Nikita Prodan <sa_vvk@mail.ru>
2022-10-28 03:43:40 +0300

    Change znimation between screens

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -10,42 +10,75 @@ function pageAnimateIn(container, text) {
   const $loader = container.querySelector('.loader');
   const $loaderText = container.querySelector('.loader__text');
   $loaderText.innerHTML = text;
+
   return tl
-    .to($loader, {
-      duration: 0.5,
-      width: '100%',
-    })
+    .add('start')
+    .to(
+      $loader,
+      {
+        duration: 0.5,
+        width: '100%',
+      },
+      'start'
+    )
+    .fromTo(
+      $loaderText,
+      {
+        transform: 'translate(-50%, 100px)',
+        opacity: 0,
+      },
+      {
+        duration: 0.5,
+        opacity: 1,
+        transform: 'translate(-50%, 0px)',
+      },
+      'start'
+    )
     .to($loaderText, {
       duration: 0.5,
-      delay: 0.25,
       opacity: 1,
-      transform: 'translate(-50%, -50%)',
-    })
-    .to($loaderText, {
-      duration: 0.5,
-      delay: 0.25,
-      opacity: 1,
-      transform: 'translate(-50%, -50%)',
-    })
-    .to($loaderText, {
-      duration: 0.5,
-      delay: 0.25,
-      opacity: 0,
-      transform: 'translate(-50%, -150%)',
+      transform: 'translate(-50%, 0px)',
     });
 }
 
-async function pageAnimateOut(container) {
-  await tl.fromTo(
-    container.querySelector('.loader'),
-    {
-      duration: 0.5,
-      width: '100%',
-    },
-    {
-      width: '0',
-    }
-  );
+async function pageAnimateOut(container, text) {
+  const $loader = container.querySelector('.loader');
+  const $loaderText = container.querySelector('.loader__text');
+  $loaderText.innerHTML = text;
+
+  await tl
+    .add('end')
+    .fromTo(
+      $loaderText,
+      {
+        duration: 0.5,
+        opacity: 1,
+        transform: 'translate(-50%, 0px)',
+      },
+      {
+        duration: 0.5,
+        opacity: 0,
+        transform: 'translate(-50%, -100px)',
+      },
+      'end'
+    )
+    .fromTo(
+      $loader,
+      {
+        duration: 0.5,
+        width: '100%',
+        left: 0,
+        right: '100%',
+      },
+      {
+        width: '0',
+        left: '100%',
+        right: 0,
+      },
+      'end'
+    )
+    .to($loader, { duration: 0, left: 0, right: '100%' });
+  // .to($loaderText, { duration: 0, transform: 'translate(-50%, 50px)' });
 }
 
 barba.init({
@@ -59,7 +92,7 @@ barba.init({
         data.current.container.remove();
       },
       async enter(data) {
-        await pageAnimateOut(data.next.container);
+        await pageAnimateOut(data.next.container, data.trigger.dataset.loader);
       },
     },
   ],
